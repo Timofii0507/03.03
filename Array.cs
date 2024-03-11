8944abc4a7758c202718283aa4c9f580b2f0847f
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-11 15:30:44 +0200

    Перше завдання  з  практичної  роботи

---
+++ Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03
+{
+    public class Array : IOutput
+    {
+        private int[] _array;
+
+        public Array(int[] array)
+        {
+            _array = array;
+        }
+
+        public void Show()
+        {
+            foreach (var element in _array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            var array = new Array(new[] { 1, 2, 3, 4, 5 });
+            array.Show();
+
+            Console.WriteLine();
+            array.Show("Інформаційне повідомлення");
+        }
+    }
+}
