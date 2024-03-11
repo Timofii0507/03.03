a127b7dbc6c600e55a92c2efb61ad1c5e007e8ba
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-11 15:47:12 +0200

    Друге завдання  з  практичної роботи

---
+++ Array2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03
+{
+    public class Array2 : IMath
+    {
+        private readonly int[] _data;
+
+        public Array2(int[] data)
+        {
+            _data = data;
+        }
+
+        public int Max()
+        {
+            int max = _data[0];
+            for (int i = 1; i < _data.Length; i++)
+            {
+                if (_data[i] > max)
+                {
+                    max = _data[i];
+                }
+            }
+
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = _data[0];
+            for (int i = 1; i < _data.Length; i++)
+            {
+                if (_data[i] < min)
+                {
+                    min = _data[i];
+                }
+            }
+
+            return min;
+        }
+
+        public float Avg()
+        {
+            float sum = 0;
+            foreach (int value in _data)
+            {
+                sum += value;
+            }
+
+            return sum / _data.Length;
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i] == valueToSearch)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] data = { 1, 5, 3, 2, 4 };
+            Array2 array = new Array2(data);
+
+            Console.WriteLine("Максимальне значення: " + array.Max());
+            Console.WriteLine("Мінімальне значення: " + array.Min());
+            Console.WriteLine("Середнє значення: " + array.Avg());
+            Console.WriteLine("Значення 3 знайдено: " + array.Search(3));
+            Console.WriteLine("Значення 7 знайдено: " + array.Search(7));
+        }
+    }
+}
